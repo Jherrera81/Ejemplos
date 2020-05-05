41d374b4d6c1e83975cd77038138e0c867e9ae12
Jherrera81 <jose210281@gmail.com>
2020-05-05 17:09:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+  //comentario
+    Scanner reader = new Scanner(System.in);
+    int numero1 = 0;
+    int numero2 = 0;
+    float resultado;
+	
+    System.out.println("Introduce el primer número:");			
+    numero1 = reader.nextInt();
+          
+    System.out.println("Introduce el segundo número:");
+    numero2 = reader.nextInt();
+    resultado = numero1+numero2;
+    System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado);
+  }
+}
